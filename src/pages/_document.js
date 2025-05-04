59c59f9b24d428b9818db0531b7fedff0f7a52b9
GersonSA01 <gsuareza3@unemi.edu.ec>
2025-05-04 17:28:03 -0500

    Logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        <title>OpenAIProjects - GersonSA</title>
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+        </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
