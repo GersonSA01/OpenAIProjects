60cc7b91efe7c64b8379eddacdc1f668b1eebb7f
GersonSA01 <gsuareza3@unemi.edu.ec>
2025-05-04 17:22:19 -0500

    Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,22 @@ import { FiMail } from 'react-icons/fi';
 
 export default function Navbar() {
   return (
-    <nav className="fixed w-full py-4 z-50 backdrop-blur-md shadow-lg">
-      <div className="flex justify-between items-center max-w-screen-xl mx-6 xl:mx-auto">
-        <a href="/" className="text-xl font-bold text-blue-600 hover:text-blue-800 transition">
+<nav  className="fixed w-full py-4 z-50 backdrop-blur-md shadow-lg">
+<div className="flex justify-between items-center max-w-screen-xl mx-14 xl:mx-auto">
+      <a href="/" className="flex items-center gap-2">
+        <img src="/logo.png" alt="Logo" className="h-10 w-10 object-contain" />
+        <span className="text-xl font-bold text-blue-500 transition">
           GersonSA
-        </a>
+        </span>
+      </a>
+
+
+
 
         <div className="hidden lg:flex gap-6 items-center">
           <a
             href="#contacto"
-            className="flex items-center gap-2 text-lg font-semibold text-gray-600 hover:text-blue-600 transition"
+            className="flex items-center gap-2 text-lg font-semibold text-blue-500 hover:text-blue-600 transition"
           >
             <FiMail className="text-xl" />
             Contacto
